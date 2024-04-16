f8ca1889a4d7a378debc2e0489c8d31085733066
Santiago Gallego H <santiago.gallegoh@upb.edu.co>
2024-04-15 22:38:43 -0500

    init

---
+++ src/TallerOpcional3.java
@@ -0,0 +1,18 @@
+public class TallerOpcional3 {
+
+
+    //Puntos2D[] leerPuntos(String filename){
+
+
+        //return
+    //}
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
